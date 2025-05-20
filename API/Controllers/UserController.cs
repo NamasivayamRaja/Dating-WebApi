9c57dfa93f6cb9ffc8aa2747b0dfa713daa08595
Namasivayam Raja <namasivayamrajamsc@gmail.com>
2025-05-20 06:57:36 +0530

    Project created and basic setup created #1

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public UserController(DataContext dataContext) 
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _dataContext.Users.ToListAsync();
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await _dataContext.Users.FindAsync(id);
+
+            if (user == null) 
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+    }
+}
---
+++ API/Models/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Models
+{
+    public class DataContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
